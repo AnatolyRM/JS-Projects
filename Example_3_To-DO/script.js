346de3b52b490a8c7d6d553c959aa39a7a0ed29a
AnatolyRM <1neytron@gmail.com>
2022-11-06 19:48:51 +0300

    init: new project

--- Example_3_To-DO/script.js
+++ Example_3_To-DO/script.js
@@ -4,56 +4,56 @@ let todoList = document.querySelector("#list");
 
 const addTodo = () => {
 
-    if(todoText.value) {
-        todoList.innerHTML += 
-        `
+  if (todoText.value) {
+    todoList.innerHTML +=
+      `
         <li class="todoli" onclick="madeList(this)"> ${todoText.value}
         <span class="mark" onclick="deleteFunc(this)" id="mark"> <i class="fa-solid fa-xmark" ></i>
         </span></li>
         `
-        todoText.value=""
-        toastyFuncSuccess()
-    }
-    else {
-        toastyFuncFail("Listeye Boş Ekleme Yapamazsınız !");
-    }
+    todoText.value = ""
+    toastyFuncSuccess()
+  }
+  else {
+    toastyFuncFail("You cannot add an empty list to the list.!");
+  }
 }
 
 const deleteFunc = (element) => {
-    element.parentElement.remove()
-    toastyFuncFail("Listeden Kaldırıldı!");
+  element.parentElement.remove()
+  toastyFuncFail("Removed from List");
 }
 
-const madeList = (element) =>{
-    if (element.classList.contains("checked")) {
-        element.classList.remove("checked");
-    }
-    else{
-        element.classList.add("checked");
-    }
+const madeList = (element) => {
+  if (element.classList.contains("checked")) {
+    element.classList.remove("checked");
+  }
+  else {
+    element.classList.add("checked");
+  }
 }
 
 // Toast //
 let toastAnimation = {
-    animation : true,
-    delay : 2000
+  animation: true,
+  delay: 2000
 };
 
-const toastyFuncSuccess  = () => {
+const toastyFuncSuccess = () => {
 
-    let toastHTMLElement = document.querySelector("#liveToast");
-    let toastElement = new bootstrap.Toast(toastHTMLElement,toastAnimation);
-    toastElement.show()
- 
+  let toastHTMLElement = document.querySelector("#liveToast");
+  let toastElement = new bootstrap.Toast(toastHTMLElement, toastAnimation);
+  toastElement.show()
+
 }
 
-const toastyFuncFail  = (text) => {
+const toastyFuncFail = (text) => {
 
-    let toastHTMLElement = document.querySelector("#liveToastError");
-    let toastElement = new bootstrap.Toast(toastHTMLElement,toastAnimation);
-    let toastElementValue = document.querySelector("#fail");
-    toastElementValue.innerHTML = `${text}`
-    toastElement.show()
+  let toastHTMLElement = document.querySelector("#liveToastError");
+  let toastElement = new bootstrap.Toast(toastHTMLElement, toastAnimation);
+  let toastElementValue = document.querySelector("#fail");
+  toastElementValue.innerHTML = `${text}`
+  toastElement.show()
 }
 
-addButton.addEventListener("click",addTodo)
+addButton.addEventListener("click", addTodo)
